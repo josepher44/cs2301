d9e30541ef306dd3074b81ed6be9c57c4ab54e9f
josepher44 <joedgallagher@yahoo.com>
2018-05-15 17:44:54 -0400

    Initial commit, lab 1 complete

---
+++ cs2301_labs/lab1/lab1.c
@@ -0,0 +1,15 @@
+/* Example C program for CS2301 Lab 1 */
+/* Created by Joe Gallagher, joedgallagher */
+#include <stdio.h>
+int main()
+{
+  int i;  // Loop counter
+
+  printf("Hello, World!\n"); //Print a cheery greeting!
+  /* This loop prints the numbers from 1 to 15, one per line. */
+  for (i = 1; i <=15; i++)
+  {
+    printf("%d\n", i); // Print the next number
+  }
+  printf("Goodbye, World!\n");
+}
