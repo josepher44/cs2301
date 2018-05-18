86a45d240ce265d31d85c08c23bfa4e74b958ca8
josepher44 <joedgallagher@yahoo.com>
2018-05-18 02:17:29 -0400

    Added leap year code for homework 1

---
+++ cs2301_hw/hw1/leap.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define cutoffYear (1752)
+/*
+ * Program to determine if an input year is a leap year
+ * Only works for years which the modern rule applies to, from 1752 on.
+ * Author: Joe Gallagher
+ * joedgallagher@wpi.edu
+ * Last modified: 5/17/2018
+ */
+int main (int argc, const char* argv[])
+{
+    //Declare variables
+    int year; // Year read from the command line goes here
+
+    // Check that there was an entry, with only one argument
+    if (argc != 2)
+    {
+        printf("Must enter a year on the command line as an argument, "
+        "as the only argument. For example: ./leap 2018\n");
+        return 1; // Indicate failure -- invalid input format
+    }
+    year = atoi(argv[1]); // Get string from command line; convert to int
+
+    if (year < cutoffYear) // Check if the year is recent enough for checking
+    {
+        printf("The year entered must be %d or more recent, as the modern "
+        "rule for leap years does not apply before this time. You entered "
+        "%d.\n", cutoffYear, year);
+        return 2; // Indicate failure -- year out of range
+    }
+
+    //Echo the year
+    printf("You entered: %d. ", year);
+
+    /*
+     * Check if a year is a leap year, using boolean logic and modulo oparators.
+     * The left side checks that the exception for years divisible by 100 unless
+     * also by 400 does not apply, the right side checks if the year is
+     * divisible by 4. If true, the year is a leap year
+     */
+    if ((year%100 != 0 || year%400 == 0) && (year%4)==0)
+    {
+        printf("The year %d is a leap year.\n", year);
+    }
+    else
+    {
+        printf("The year %d is not a leap year.\n", year);
+    }
+    return 0; // Indicate success!
+}
--- cs2301_labs/lab1/lab1.c
+++ cs2301_labs/lab1/lab1.c
@@ -12,4 +12,22 @@ int main()
     printf("%d\n", i); // Print the next number
   }
   printf("Goodbye, World!\n");
+
+
+  int p;
+  int j;
+  int k;
+
+  p = 0;
+  while (p < 5) {
+    j = p++;
+  }
+
+  printf("%d\n", j); // Print the next number
+  p = 0;
+  do {
+    k = --j;
+  } while (j > 0);
+  printf("%d\n", j); // Print the next number
+  printf("%d\n", k); // Print the next number
 }
